e1a7d4482998d5d529710a2676df0ca28115ad75
Tung Vu <tungvuduc2805@gmail.com>
2023-07-03 14:45:58 +0700

    init project

---
+++ GithubActionsConfiguration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubActionsConfiguration
+//
+//  Created by Tung Vu Duc on 03/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GithubActionsConfigurationTests/GithubActionsConfigurationTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsConfigurationTests.swift
+//  GithubActionsConfigurationTests
+//
+//  Created by Tung Vu Duc on 03/07/2023.
+//
+
+import XCTest
+@testable import GithubActionsConfiguration
+
+final class GithubActionsConfigurationTests: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertEqual(2*2, 4)
+    }
+
+}
